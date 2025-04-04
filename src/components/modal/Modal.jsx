bd16fed2b3e06c18666cc987dbcbefb620d43139
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-04-04 15:48:44 +0200

    Added: modal component. Library: react-modal

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,29 @@
+import Modal from "react-modal"
+import PropTypes from "prop-types"
+import "./modal.css"
+
+const ModalComponent = ({ isOpen, onRequestClose }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Employee successfully created modal"
+      aria={{ labelledby: "heading" }}
+      className="modal"
+      overlayClassName=""
+    >
+        <div className="text-container">
+            <h2 id="heading">Employee Created!</h2>
+            
+            <button onClick={onRequestClose} className="button">Close</button>
+        </div>
+    </Modal>
+  )
+}
+
+ModalComponent.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    onRequestClose: PropTypes.func.isRequired,
+}
+
+export default ModalComponent
--- src/pages/create-employee/CreateEmployee.jsx
+++ src/pages/create-employee/CreateEmployee.jsx
@@ -1,40 +1,76 @@
+import { useState } from "react"
+import ModalComponent from "../../components/modal/Modal"
 import "./createEmployee.css"
 
 function CreateEmployeePage() {
+
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    setModalIsOpen(true)
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  console.log(modalIsOpen)
+
   return (
     <div className="create-employee-container">
       
       <h1>Create Employee</h1>
 
-      <form id="create-employee">
+      <form id="create-employee" onSubmit={handleSubmit}>
+
+        {/* Faire un composant de ça */}
+        <div className="label-input">
           <label htlmfor="first-name">First Name</label>
           <input type="text" id="first-name" />
+        </div>
 
-          <label htlmfor="last-name">Last Name</label>
+        <div className="label-input">
+          <label htmlFor="last-name">Last Name</label>
           <input type="text" id="last-name" />
+        </div>
 
-          <label htlmfor="date-of-birth">Date of Birth</label>
+        <div className="label-input">
+          <label htmlFor="date-of-birth">Date of Birth</label>
           <input id="date-of-birth" type="text" />
+        </div>
 
-          <label htlmfor="start-date">Start Date</label>
+        <div className="label-input">
+          <label htmlFor="start-date">Start Date</label>
           <input id="start-date" type="text" />
+        </div>
 
+          {/* Address */}
           <fieldset className="address">
               <legend>Address</legend>
 
-              <label htlmfor="street">Street</label>
-              <input id="street" type="text" />
+              <div className="label-input">
+                <label htmlFor="street">Street</label>
+                <input id="street" type="text" />
+              </div>
 
-              <label htlmfor="city">City</label>
-              <input id="city" type="text" />
+              <div className="label-input">
+                <label htmlFor="city">City</label>
+                <input id="city" type="text" />
+              </div>
 
-              <label htlmfor="state">State</label>
-              <select name="state" id="state"></select>
+              <div className="label-input">
+                <label htmlFor="state">State</label>
+                <select name="state" id="state"></select>
+              </div>
 
-              <label htlmfor="zip-code">Zip Code</label>
-              <input id="zip-code" type="number" />
+              <div className="label-input">
+                <label htmlFor="zip-code">Zip Code</label>
+                <input id="zip-code" type="number" />
+              </div>
           </fieldset>
 
+          {/* Department */}
           <label htlmfor="department">Department</label>
           <select name="department" id="department">
               <option>Sales</option>
@@ -43,9 +79,13 @@ function CreateEmployeePage() {
               <option>Human Resources</option>
               <option>Legal</option>
           </select>
+
+        <button type="submit" className="button">Save</button>
+        
       </form>
 
-      <button className="button">Save</button>
+       {/* Modal component after submitting a new employee */}
+       <ModalComponent isOpen={modalIsOpen} onRequestClose={closeModal} />
 
     </div>
   )
