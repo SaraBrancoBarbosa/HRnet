406954877bff40f8f0029a93e160bd4b13f10c0e
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-04-10 18:51:03 +0200

    Customisation of the dropdown component

---
+++ src/components/dropdown/Dropdown.jsx
@@ -0,0 +1,48 @@
+import Select from "react-select"
+import PropTypes from "prop-types"
+
+const customStyles = {
+  control: (baseStyles, state) => ({
+    ...baseStyles,
+    height: 21,
+    minHeight: 21,
+    maxWidth: 186,
+    minWidth: 186,
+    alignContent: "center",
+    borderColor: state.isFocused ? "#0060DF" : "#8F8F9D",
+    boxShadow: state.isFocused ? "0 0 0 1px #0060DF" : "none",
+    borderRadius: 2,
+    paddingLeft: 0,
+    "&:hover": {
+      borderColor: state.isFocused ? "#0060DF" : "#8F8F9D",
+      
+    }
+  }),
+}
+
+function DropdownComponent({ id, label, options, isLoading, isDisabled, onChange }) {
+  return (
+    <Select
+        id={id}
+        label={label}
+        options={options}
+        isLoading={isLoading}
+        isDisabled={isDisabled}
+        onChange={onChange}
+        placeholder={`Select a ${label.toLowerCase()}`}
+        getOptionLabel={(e) => e.label}
+        styles={customStyles}
+    />
+  )
+}
+
+DropdownComponent.propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    options: PropTypes.array.isRequired,
+    isLoading: PropTypes.bool.isRequired,
+    isDisabled: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired,
+}
+
+export default DropdownComponent
