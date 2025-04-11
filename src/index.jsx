c6873f0de01ed6874ace96e7beb2df9b0e3801b6
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-04-11 16:29:53 +0200

    ApiProvider new path

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import ApiProviderEmployees from "./api/employee-context/ApiProvider.jsx"
+import App from "./App.jsx"
 import Modal from "react-modal"
 import "./index.css"
-import App from "./App.jsx"
-import ApiProviderEmployees from "./api/context/ApiProvider.jsx"
 
 Modal.setAppElement("#root")
 
