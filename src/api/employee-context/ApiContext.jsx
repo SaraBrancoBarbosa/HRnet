498e3e79d02b60d23004adf08c1f81a95169d8de
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-04-11 16:28:39 +0200

    Context name more explicit

--- src/api/employee-context/ApiContext.jsx
+++ src/api/employee-context/ApiContext.jsx
@@ -5,5 +5,5 @@ Create a React context. Used to share the state throughout the app
 Initialisation: "null"
 *****/
 
-const Context = createContext(null)
-export default Context
+const EmployeeContext = createContext(null)
+export default EmployeeContext
