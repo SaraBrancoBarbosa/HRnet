0e278648201890d9e264b81ba3beacf66439ed4a
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-04-18 14:23:39 +0200

    Modal update: use of generics

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -2,18 +2,22 @@ import Modal from "react-modal"
 import PropTypes from "prop-types"
 import "./modal.css"
 
-const ModalComponent = ({ isOpen, onRequestClose }) => {
+const ModalComponent = ({ isOpen, onRequestClose, title, message, children }) => {
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-      contentLabel="Employee successfully created modal"
       aria={{ labelledby: "heading" }}
       className="modal"
     >
         <div className="text-container">
-            <h2 id="heading">Employee created!</h2>
-            <button onClick={onRequestClose} className="button">Close</button>
+            <h2 id="heading">{title}</h2>
+            {message && <p>{message}</p>}
+
+            {/* Children parameters to customise the buttons */}
+            <div className="modal-buttons">
+              {children}
+            </div>
         </div>
     </Modal>
   )
@@ -22,6 +26,9 @@ const ModalComponent = ({ isOpen, onRequestClose }) => {
 ModalComponent.propTypes = {
     isOpen: PropTypes.bool.isRequired,
     onRequestClose: PropTypes.func.isRequired,
+    title: PropTypes.string.isRequired,
+    message: PropTypes.string,
+    children: PropTypes.node,
 }
 
 export default ModalComponent
