b7e136ad3b15082c1e48582a7bb7fab0a75c5602
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-05-02 16:27:24 +0200

    Props names + comments update

--- src/pages/employees-list/EmployeesList.jsx
+++ src/pages/employees-list/EmployeesList.jsx
@@ -65,6 +65,12 @@ function EmployeesListPage() {
         headers={headers} 
         rows={data} 
         deleteRow={deleteEmployee}
+        // If you don't want to use one of these features, enter {false}
+        showPagination = {true}
+        showSearchBar = {true}
+        showSortItem = {true}
+        showDeleteItem = {true}
+        // If you have your own modal/a modal library, you can use it to confirm or cancel the deletion. If you don't, the deletion will be immediate.
         modalComponent={customModal}
       />
 
