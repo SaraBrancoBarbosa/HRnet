85e7516ecbb94d646d68300189e72be2423f44dc
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2025-05-09 11:18:50 +0200

    Indent corrections

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -10,25 +10,25 @@ const ModalComponent = ({ isOpen, onRequestClose, title, message, children }) =>
       aria={{ labelledby: "heading" }}
       className="modal"
     >
-        <div className="text-container">
-            <h2 id="heading">{title}</h2>
-            {message && <p>{message}</p>}
+      <div className="text-container">
+        <h2 id="heading">{title}</h2>
+        {message && <p>{message}</p>}
 
-            {/* Children parameters to customise the buttons */}
-            <div className="modal-buttons">
-              {children}
-            </div>
+        {/* Children parameters to customise the buttons */}
+        <div className="modal-buttons">
+          {children}
         </div>
+      </div>
     </Modal>
   )
 }
 
 ModalComponent.propTypes = {
-    isOpen: PropTypes.bool.isRequired,
-    onRequestClose: PropTypes.func.isRequired,
-    title: PropTypes.string.isRequired,
-    message: PropTypes.string,
-    children: PropTypes.node,
+  isOpen: PropTypes.bool.isRequired,
+  onRequestClose: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  children: PropTypes.node,
 }
 
 export default ModalComponent
